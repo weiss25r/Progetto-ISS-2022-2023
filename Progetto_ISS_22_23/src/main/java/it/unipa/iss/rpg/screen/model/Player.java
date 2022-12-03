2cde74e13f2370c50d13f1c3d79e2718627ddcd4
Raffaele Terracino <raffaeleterracino001@gmail.com>
2022-12-03 10:32:57 +0100

    Minor changes

---
+++ Progetto_ISS_22_23/src/main/java/it/unipa/iss/rpg/screen/model/Player.java
@@ -0,0 +1,15 @@
+package it.unipa.iss.rpg.screen.model;
+
+import java.util.*;
+
+public class Player {
+    private int worldX;
+    private int worldY;
+    private List<Tile> playerSprites;
+    private int movementSpeed;
+
+    
+
+
+
+}
