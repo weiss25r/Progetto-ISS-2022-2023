fd078a0ec0ef244bcd9a0629fee7227a1fffa4ba
ArgonautAstra <78497325+ArgonautAstra@users.noreply.github.com>
2022-12-16 10:42:28 +0100

    Update

--- Progetto_ISS_22_23/src/main/java/it/unipa/iss/rpg/screen/view/CombatPanel.java
+++ Progetto_ISS_22_23/src/main/java/it/unipa/iss/rpg/screen/view/CombatPanel.java
@@ -38,13 +38,13 @@ public class CombatPanel extends GamePanel{
         stats = new JPanel();
         stats.setBackground(Color.black);
         stats.setLayout(new GridLayout(2,5));
-        this.lblPlayerHp = new JLabel("100/100");
+        this.lblPlayerHp = new JLabel("HP: 100/100");
         this.lblPlayerHp.setForeground(Color.white);
         this.lblPlayerHp.setFont(new Font("Arial", Font.PLAIN, 20));
-        this.lblPlayerStamina = new JLabel("50/50");
+        this.lblPlayerStamina = new JLabel("SP: 50/50");
         this.lblPlayerStamina.setForeground(Color.white);
         this.lblPlayerStamina.setFont(new Font("Arial", Font.PLAIN, 20));
-        this.lblEnemyHp = new JLabel("100/100");
+        this.lblEnemyHp = new JLabel("HP: 100/100");
         this.lblEnemyHp.setForeground(Color.white);
         this.lblEnemyHp.setFont(new Font("Arial", Font.PLAIN, 20));
         stats.add(lblPlayerHp);
