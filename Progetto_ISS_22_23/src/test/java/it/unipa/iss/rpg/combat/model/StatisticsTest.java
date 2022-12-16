ee3d844399f5e9449f7110ce22cf5967a324a618
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2022-12-16 11:21:45 +0100

    TestStatisticsChange
    Test classe stattistics

--- Progetto_ISS_22_23/src/test/java/it/unipa/iss/rpg/combat/model/StatisticsTest.java
+++ Progetto_ISS_22_23/src/test/java/it/unipa/iss/rpg/combat/model/StatisticsTest.java
@@ -6,35 +6,71 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class StatisticsTest {
+    private Statistics stats;
+    private Statistics v;
+
 
     @BeforeEach
     void setUp() {
+        //setting up a Statistic object to test the metod get
+        stats = new Statistics(100,20,50,20);
+
     }
 
 
     @AfterEach
     void tearDown() {
     }
-    @Test
-    void shouldICreate(){
-        Statistics stat = new Statistics();
-        assertEquals(-1,stat.getAtk());
-        assertEquals(-1,stat.getHp());
-        assertEquals(-1,stat.getStamina());
-        assertEquals(-1,stat.getDef());
-
-
-    }
-
 
 
 
     @Test
     void testClone() {
+        try{
+             this.v = (Statistics) stats.clone();
+        }catch(CloneNotSupportedException e){
+            System.out.println("Error clone");
+        }
+        assertTrue(this.v instanceof Statistics);
     }
 
     @Test
     void attack() {
     }
 
+    @Test
+    void setHp() {
+    }
+
+    @Test
+    void setDef() {
+    }
+
+    @Test
+    void setStamina() {
+    }
+
+    @Test
+    void setAtk() {
+    }
+
+    @Test
+    void getHp() {
+        assertEquals(100,stats.getHp());
+    }
+
+    @Test
+    void getDef() {
+        assertEquals(20,stats.getDef());
+    }
+
+    @Test
+    void getStamina() {
+        assertEquals(50,stats.getStamina());
+    }
+
+    @Test
+    void getAtk() {
+        assertEquals(20,stats.getAtk());
+    }
 }
