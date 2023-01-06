41f723f3731117859a8b2c21e2cb641d29364d80
Alessia Boni <118371511+AleRaylee@users.noreply.github.com>
2023-01-06 12:27:53 +0100

    CombatControllerTest

--- Progetto_ISS_22_23/src/test/java/it/unipa/iss/rpg/combat/controller/CombatControllerTest.java
+++ Progetto_ISS_22_23/src/test/java/it/unipa/iss/rpg/combat/controller/CombatControllerTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CombatControllerTest {
@@ -26,38 +30,74 @@ class CombatControllerTest {
        this.enemy =  new Mob(this.stats);
        this.view  = new CombatPanel();
        this.player = new Player();
-       //Test if the excpetion work perfectly
 
-       //assertDoesNotThrow(CloneNotSupportedException.class,()->ftest = new Fight(this.player,this.enemy));
        this.controller = new CombatController(this.player,this.view,this.enemy);
 
     }
     @Test
     void updateTest(){
 
+        /*
+        Pattern pattern = Pattern.compile("");
+        Matcher matchHero = pattern.matcher(HpHero);
+        int startHpHero = matchHero.start();
+        int endHpHero = matchHero.end();
+        String HpHero = controller.getView().getLblPlayerHp().getText();
+        String HpEnemy = controller.getView().getLblEnemyHp().getText();
+        String PlayerStamina = controller.getView().getLblPlayerStamina().getText();
+         */
+
         this.controller.getFight().inputAction(1);
         //this.controller.getFight().getHpHeroRemaining();
         this.controller.update(EventType.PLAYER_ATK);
 
-        assertEquals(Integer.toString(controller.getFight().getHpHeroRemaining()),
-                     controller.getView().getLblPlayerHp().getText());
-        assertEquals(Integer.toString(controller.getFight().getHpEnemyRemaining()),
-                controller.getView().getLblEnemyHp().getText());
+        assertEquals(Integer.toString(this.controller.getFight().getHpHeroRemaining()),
+                controller.getView().getLblPlayerHp().getText().substring(11,controller.getView().getLblPlayerHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getHpEnemyRemaining()),
+                controller.getView().getLblEnemyHp().getText().substring(10,controller.getView().getLblEnemyHp().getText().length()));
+
+        //Test Corruptive_Atk
+        this.controller.getFight().setSkill(AbilityBox.CORRUPTIVE_ATK);
+        this.controller.getFight().inputAction(2);
+        this.controller.update(EventType.PLAYER_ABILITY);
+        assertEquals(Integer.toString(this.controller.getFight().getHpHeroRemaining()),
+                controller.getView().getLblPlayerHp().getText().substring(11,controller.getView().getLblPlayerHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getHpEnemyRemaining()),
+                controller.getView().getLblEnemyHp().getText().substring(10,controller.getView().getLblEnemyHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getStaminaHeroRemaining()),
+                controller.getView().getLblPlayerStamina().getText().substring(15,controller.getView().getLblPlayerStamina().getText().length()));
+
 
         //Test sinner atk
         this.controller.getFight().setSkill(AbilityBox.SINNER_ATK);
         this.controller.getFight().inputAction(2);
         this.controller.update(EventType.PLAYER_ABILITY);
-        assertEquals(Integer.toString(controller.getFight().getHpHeroRemaining()),
-                controller.getView().getLblPlayerHp().getText());
-        assertEquals(Integer.toString(controller.getFight().getHpEnemyRemaining()),
-                controller.getView().getLblEnemyHp().getText());
-        assertEquals(Integer.toString(controller.getFight().getStaminaHeroRemaining()),
-                    controller.getView().getLblPlayerStamina());
+        assertEquals(Integer.toString(this.controller.getFight().getHpHeroRemaining()),
+                controller.getView().getLblPlayerHp().getText().substring(11,controller.getView().getLblPlayerHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getHpEnemyRemaining()),
+                controller.getView().getLblEnemyHp().getText().substring(10,controller.getView().getLblEnemyHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getStaminaHeroRemaining()),
+                        controller.getView().getLblPlayerStamina().getText().substring(15,controller.getView().getLblPlayerStamina().getText().length()));
+
+        //Test StudyPower
+        this.controller.getFight().setSkill(AbilityBox.STUDY_POWER);
+        this.controller.getFight().inputAction(2);
+        this.controller.update(EventType.PLAYER_ABILITY);
+        assertEquals(Integer.toString(this.controller.getFight().getHpHeroRemaining()),
+                controller.getView().getLblPlayerHp().getText().substring(11,controller.getView().getLblPlayerHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getHpEnemyRemaining()),
+                controller.getView().getLblEnemyHp().getText().substring(10,controller.getView().getLblEnemyHp().getText().length()));
+        assertEquals(Integer.toString(this.controller.getFight().getStaminaHeroRemaining()),
+                controller.getView().getLblPlayerStamina().getText().substring(15,controller.getView().getLblPlayerStamina().getText().length()));
+
+
+
         //Test Healing
         this.controller.getFight().setSkill(AbilityBox.HEALING);
         this.controller.getFight().inputAction(2);
         this.controller.update(EventType.PLAYER_ABILITY);
+        assertEquals(Integer.toString(this.controller.getFight().getHpHeroRemaining()),
+                controller.getView().getLblPlayerHp().getText().substring(11,controller.getView().getLblPlayerHp().getText().length()));
 
 
     }
