808621e84b2b6e0e7a62ff8ee1c8e2b49623d348
Jonnhy98 <117198764+Jonnhy98@users.noreply.github.com>
2023-01-07 09:39:33 +0100

    Test connection to DB

--- Progetto_ISS_22_23/src/main/java/it/unipa/iss/rpg/DAO/EntityDAO.java
+++ Progetto_ISS_22_23/src/main/java/it/unipa/iss/rpg/DAO/EntityDAO.java
@@ -1,13 +1,11 @@
 package it.unipa.iss.rpg.DAO;
 
-import it.unipa.iss.rpg.combat.model.Statistics;
 import it.unipa.iss.rpg.screen.model.entitities.Mob;
+import it.unipa.iss.rpg.screen.model.entitities.Npc;
 import it.unipa.iss.rpg.screen.model.entitities.Player;
 
-import java.util.ArrayList;
-
 public interface EntityDAO {
-    public Player getHeroStatsById(String id);
+    public Player getHeroById(String id);
     public Mob getEnemyById(String id);
-    public Statistics getStatsById(String id);
+    public Npc getNpcById(String id);
 }
