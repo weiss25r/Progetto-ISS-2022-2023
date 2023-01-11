c1b91da298145b6b75642f35260528851e03a215
Raffaele <raffaeleterracino001@gmail.com>
2023-01-11 16:59:25 +0100

    relazione

---
+++ Progetto_ISS_22_23/src/test/java/it/unipa/iss/rpg/screen/controller/LevelLoaderTest.java
@@ -0,0 +1,22 @@
+package it.unipa.iss.rpg.screen.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelLoaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void loadLevelTest() {
+
+    }
+
+    @Test
+    void loadPlayer() {
+    }
+}
